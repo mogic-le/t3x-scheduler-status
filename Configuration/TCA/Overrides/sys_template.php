3f30be904e9e428d808bdbc92a3b4b0a7664d6ca
Christian Weiske <weiske@mogic.com>
2023-04-28 13:07:06 +0200

    MGI-556: Rename extension and namespace

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,6 @@
 <?php
 defined('TYPO3') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('typo3_scheduler_monitoring', 'Configuration/TypoScript', 'Scheduler Monitoring');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'scheduler_status', 'Configuration/TypoScript', 'Scheduler status'
+);
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,36 +1,14 @@
 <?php
 defined('TYPO3') || die();
 
-(static function() {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Typo3SchedulerMonitoring',
-        'Monitor',
-        [
-            \Mogic\Typo3SchedulerMonitoring\Controller\MonitorController::class => 'list'
-        ],
-        // non-cacheable actions
-        [
-            \Mogic\Typo3SchedulerMonitoring\Controller\MonitorController::class => 'list'
-        ]
-    );
-
-    // wizards
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        'mod {
-            wizards.newContentElement.wizardItems.plugins {
-                elements {
-                    monitor {
-                        iconIdentifier = typo3_scheduler_monitoring-plugin-monitor
-                        title = Scheduler Monitoring
-                        description = Json response for created Scheduler Jobs
-                        tt_content_defValues {
-                            CType = list
-                            list_type = typo3schedulermonitoring_monitor
-                        }
-                    }
-                }
-                show = *
-            }
-       }'
-    );
-})();
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'SchedulerStatus',
+    'Monitor',
+    [
+        \Mogic\SchedulerStatus\Controller\MonitorController::class => 'list'
+    ],
+    // non-cacheable actions
+    [
+        \Mogic\SchedulerStatus\Controller\MonitorController::class => 'list'
+    ]
+);
